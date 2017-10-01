ada03ba9dee502013c6f48955fda85dd1d6ec2d7
Brian Quiroz <r229q057@ku.edu>
2017-10-01 17:11:55 -0500

    Removed set and get orientation
    Orientation is not necessary

--- RubiksCubeSolver/Piece.h
+++ RubiksCubeSolver/Piece.h
@@ -7,9 +7,7 @@ class Piece {
 	public:
 		Piece();
 		std::string getType() const;
-		int getOrientation() const;
 		int getPermutation() const;
-		void setOrientation(int orientation);
 		void setPermutation(int permutation);
 	protected:
 		int p_orientation;
